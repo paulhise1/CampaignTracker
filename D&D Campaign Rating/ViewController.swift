4a22887859f01b36f2917ff1e0aa7f9bbedd7c75
CJ McCaskill <cjmccaskill@gmail.com>
2021-01-25 19:54:44 -0600

    Initial Commit

---
+++ D&D Campaign Rating/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  D&D Campaign Rating
+//
+//  Created by CJ McCaskill on 1/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
